f85f9abef35085f0c6c56e1e03f86ff409211c5a
kinoshita0923 <sota0923ks15@gmail.com>
2021-07-07 00:27:26 +0900

    対戦画面を追加しました。

---
+++ room_connect/room.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+require_once('DB.php');
+
+$db = new DB();
+
+$room_number = $_GET['room_number'];
+
+$result = $db->room_info($room_number);
+
+if ($result === false) {
+    exit('検索した部屋は存在しません。');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "$room_number"; ?></title>
+    <style>
+        * {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <?php echo "$room_number"; ?>
+    </h1>
+</body>
+</html>
