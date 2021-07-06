e19bd3fbe28db94ee06635252994141504299f65
kinoshita0923 <sota0923ks15@gmail.com>
2021-07-07 00:36:39 +0900

    スタイルシートのリンクを削除しました。

--- room_connect/search_result.php
+++ room_connect/search_result.php
@@ -36,7 +36,6 @@ if ($result === false) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>部屋詳細</title>
-    <link rel="stylesheet" href="css\join_room.css">
 </head>
 <body>
     <div class="room">
