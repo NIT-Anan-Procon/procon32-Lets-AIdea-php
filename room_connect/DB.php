e37c444381f4e021ccd7d600a8ff332bf6459610
kinoshita0923 <sota0923ks15@gmail.com>
2021-07-07 00:13:28 +0900

    データベースに処理を行うファイルの追加

--- room_connect/DB.php
+++ room_connect/DB.php
@@ -1,55 +1,55 @@
 ﻿<?php
 
 class DB {
-    function dbConnect() {      //DB接続
-        $dsn = 'mysql:host=localhost;dbname=db_name;charset=utf8';  /* DBに接続するために必要な情報 */
-        $user = 'user_name';                                            /* DBのデータ、構造の権限を持ったユーザー名 */
-        $pass = 'password';                                           /* ユーザー名に対応したパスワード */
+    function dbConnect() {
+        $dsn = 'mysql:host=localhost;dbname=db_name;charset=utf8';
+        $user = 'user_name';
+        $pass = 'password';
         try {
             $dbh = new PDO($dsn,$user,$pass,[
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,           /* PDOクラスのオプションを設定 */ 
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             ]);
         } catch(PDOException $e) {
-            echo '接続失敗'. $e->getMessage();                          /* 接続に失敗したことを示す */
-            exit();                                                     /* プログラムを終了する */
+            echo '接続失敗'. $e->getMessage();
+            exit();
         };
         return $dbh;
     }
 
     function CreateRoomNumber() {
-        $room_number = random_int(0, 99999999);         /* 0～99999999までの数値をランダムに生成 */
-        $code = (int)(sprintf('%08d', $room_number));   /* $room_numberを8桁のint型に変更 */
-        if (empty($this->room_info($code))) {                  /* もし同じ部屋番号がなければ$codeを返す */
+        $room_number = random_int(0, 99999999);
+        $code = (int)(sprintf('%08d', $room_number));
+        if (empty($this->room_info($code))) {
             return $code;
-        } else {                                        /* もし同じ部屋番号があれば部屋番号を作り直す */
+        } else {
             $this->CreateRoomNumber();
         }
     }
 
     function create_room($room_number, $publish_status) {
-        $sql = "INSERT INTO table_name(room_number, publish_status)   /* SQL文を代入 */
+        $sql = "INSERT INTO table_name(room_number, publish_status)
         VALUES
             (:room_number, :publish_status)";
 
-        $dbh = $this->dbConnect();                             /* DB接続 */
+        $dbh = $this->dbConnect();
 
         try {
-            $stmt = $dbh->prepare($sql);                /* 文を実行する準備を行い、文オブジェクトを返す */
-            $stmt->bindValue(':room_number', $room_number, PDO::PARAM_STR);     /* $room_numberを:room_numberに結びつける */
-            $stmt->bindValue(':publish_status', $publish_status, PDO::PARAM_STR);    /* $publish_statusを:publish_statusに結びつける */
-            $stmt->execute();                           /* SQLを実行する */
-        } catch(PDOException $e) {
-            exit($e);                                   /* もしDB接続に失敗すればエラーコードを示し、プログラムを終了する */
+            $stmt = $dbh->prepare($sql);
+            $stmt->bindValue(':room_number', $room_number, PDO::PARAM_STR);
+            $stmt->bindValue(':publish_status', $publish_status, PDO::PARAM_STR);
+            $stmt->execute();
+       } catch(PDOException $e) {
+            exit($e);
         }
     }
 
     function room_info($room_number) {
-        $dbh = $this->dbConnect();                             /* DB接続 */
-        $stmt = $dbh->prepare('SELECT * FROM table_name where room_number = :room_number');   /* roomテーブルにあるroom_numberが:room_numberと同じレコードを取得する */
-        $stmt->bindValue(':room_number', $room_number, PDO::PARAM_INT);                 /* $room_numberに:room_numberを結びつける */
-        $stmt->execute();                               /* SQLを実行する */
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);       /* 実行した結果からレコードを取得する */
-        return $result;                                 /* 取得したレコードを返す */
+        $dbh = $this->dbConnect();
+        $stmt = $dbh->prepare('SELECT * FROM table_name where room_number = :room_number');
+        $stmt->bindValue(':room_number', $room_number, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
     }
 }
 
