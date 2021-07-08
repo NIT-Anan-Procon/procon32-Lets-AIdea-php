9acf3b01c11d55b3dda66588da60cb28e1ec32b8
kinoshita0923 <sota0923ks15@gmail.com>
2021-07-08 16:35:47 +0900

    unsplashAPIの実装を進めました

--- unsplash_API/unsplash_API.php
+++ unsplash_API/unsplash_API.php
@@ -1,13 +1,12 @@
 ﻿<?php
 
 require_once('../../info.php');
+require('vendor/autoload.php');
 
 $access = access;
 $secret = secret;
 $callback = callback;
 $application = application;
-
-$json = file_get_contents("composer.json");
 
 Unsplash\HttpClient::init([
 	'applicationId'	=> "$access",
