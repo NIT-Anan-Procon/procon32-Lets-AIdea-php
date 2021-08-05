9b54c2e81d49a8c04b63b86c59f062b599afac09
kinoshita0923 <sota0923ks15@gmail.com>
2021-08-05 17:25:24 +0900

    写真のurlを取得して返すようにしました。

--- unsplash_API/unsplash_API.php
+++ unsplash_API/unsplash_API.php
@@ -10,38 +10,21 @@ $application = application;
 
 Unsplash\HttpClient::init([
 	'applicationId'	=> "$access",
-	'secret'	=> "$secret",
-	'callbackUrl'	=> "$callback",
+	'secret' => "$secret",
+	'callbackUrl' => "$callback",
 	'utmSource' => "$application"
 ]);
 
 function getPhoto($word) {
-	$filter = [
-		'query' => 'sea',
-		'w'		=> '800',
-		'h' 	=> '600'
+	$filters = [
+		'query'    => "$word",
+		'w'        => 600,
+		'h'        => 400
 	];
-	$photo = Unsplash\Photo::random($filter);
+	$photo = Unsplash\Photo::random($filters);
+
 	$img = $photo->download();
 	return $img;
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<style>
-		img {
-			width: 800px;
-			height: 600px;
-		}
-	</style>
-</head>
-<body>
-	<img src="<?php echo $img; ?>">
-</body>
-</html>
