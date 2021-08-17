a92b74f2013233375d0be56d340249ae07d37aa5
KubotaShunsuke <s.kubota0921@gmail.com>
2021-08-17 20:44:43 +0900

    NGワードをライブラリに保存する機能が完成 #34

--- library/library.php
+++ library/library.php
@@ -3,13 +3,18 @@
 require_once('../../library_info.php');
 
 class library {
-    public $dbh;
+    protected $dbh;
+    protected $library_table;
+    protected $wordLibrary_table;
 
     function __construct() {
 
         $dbname = db_name;
         $password = password;
         $user_name = db_user;
+        $this->library_table = library_table;
+        $this->wordLibrary_table = wordLibrary_table;
+
         $dsn = "mysql:host=localhost;dbname=$dbname;charset=utf8";
 
         try {
@@ -27,8 +32,7 @@ class library {
         date_default_timezone_set('Asia/Tokyo');
         $today = date("Y/m/d H:i:s");
 
-        $table = table;
-        $sql = "INSERT INTO $table(userID, explanation, pictureURL, time)
+        $sql = "INSERT INTO $this->library_table(userID, explanation, pictureURL, time)
         VALUES
             (:userID, :explanation, :pictureURL, :time)";
 
@@ -47,26 +51,49 @@ class library {
     }
 
     function GetLibrary($userID) { //特定のユーザーの作品を新しいもの順で返す
-        
-        $table = table;
-;
-        $stmt = $this->dbh->prepare("SELECT * FROM $table WHERE userID = :userID ORDER BY time DESC");
+
+        $stmt = $this->dbh->prepare("SELECT * FROM $this->library_table WHERE userID = :userID ORDER BY libraryID DESC");
         $stmt->bindValue(':userID', $userID);
         $stmt->execute();
-        $result = $stmt->fetchAll();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
 
     }
 
     function ListLibrary() { //全ユーザーの作品を新着順で返す
-        
-        $table = table;
 
-        $stmt = $this->dbh->prepare("SELECT * FROM $table ORDER BY libraryID DESC");
+        $stmt = $this->dbh->prepare("SELECT * FROM $this->library_table ORDER BY libraryID DESC");
         $stmt->execute();
-        $result = $stmt->fetchAll();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
 
     }
 
+    function AddWordLibrary($libraryID, $word){
+
+        $sql = "INSERT INTO $this->wordLibrary_table(libraryID, word)
+        VALUES
+            (:libraryID, :word)";
+        try {            
+            $stmt = $this->dbh->prepare($sql);
+            $stmt->bindValue(':libraryID', $libraryID);
+            $stmt->bindValue(':word', $word);
+            $stmt->execute();
+            return true;
+
+        } catch(PDOException $e) {
+            echo '接続失敗'.$e->getMessage();
+            return false;
+            exit();
+        }
+    }
+
+    function GetWordLibrary($libraryID){
+
+        $stmt = $this->dbh->prepare("SELECT word FROM $this->wordLibrary_table WHERE libraryID = :libraryID");
+        $stmt->bindValue(':libraryID', $libraryID);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        return $result;
+    }
 }
