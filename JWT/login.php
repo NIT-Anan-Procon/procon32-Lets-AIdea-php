4e403f1a062fdebf9efe0f5de82bd549b1742540
kinoshita0923 <sota0923ks15@gmail.com>
2021-08-19 13:03:05 +0900

    cookieに保存する処理を変更しました。

--- JWT/login.php
+++ JWT/login.php
@@ -33,8 +33,8 @@ if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST') {
     
             header('Content-Type: application/json');//レスポンスする形式はJSONファイル
             header('Access-Control-Allow-Origin: *'); // CORS
-            // echo json_encode(array('token' => $jwt)); // token を返却
-            setcookie(json_encode(array('token' => $jwt)));
+            echo json_encode(array('token' => $jwt)); // token を返却
+            setcookie('token', json_encode(array('token' => $jwt)), (time() + 36), "/");
             return;
         }
     }
