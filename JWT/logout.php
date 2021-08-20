47269344d808f436438a918ad139753da1925675
kinoshita0923 <sota0923ks15@gmail.com>
2021-08-20 20:36:44 +0900

    ログアウト処理を進めました。

--- JWT/logout.php
+++ JWT/logout.php
@@ -3,12 +3,12 @@
 require __DIR__ . '/const.php';
 require __DIR__ . '/vendor/autoload.php';
 
-$jwt = $_COOKIE['token'];
-if($jwt != null) {
+if(!empty($_COOKIE['token'])) {
     echo "ログアウト";
+    $JWT = JWT::decode($jwt, JWT_KEY, ['HS256']);
+    // var_dump($JWT);
+    // echo json_encode($jwt);
+    // $JWT = json_decode($jwt);
+    setcookie('token', '', (time() + -3600), '/');
 }
-echo json_encode($jwt);
 
-$JWT = json_decode($jwt);
-
-setcookie('token', '', (time() + -3600), '/');
