4adff01e533c890f8574dba695f75e5089983281
KubotaShunsuke <s.kubota0921@gmail.com>
2021-08-22 16:44:52 +0900

    GET通信に統一 #40

--- API/UploadLibrary.php
+++ API/UploadLibrary.php
@@ -5,7 +5,6 @@ require_once('../library/library.php');
 require_once('../room_connect/room.php');
 $library = new library();
 //    $userID explanation pictureURL NGword flag
-$playerID = (int)$_POST['playerID'];
 $result = $point->AddPoint($gameID, $playerID, $pointNum, $flag);
 echo json_encode($result);
 http_response_code(200);
