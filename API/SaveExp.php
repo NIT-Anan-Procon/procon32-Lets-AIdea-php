8236c63aac24c00ea598b11df52bf161d24ecab5
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-08-27 13:38:51 +0000

    huskyとlint-stagedの追加

--- API/SaveExp.php
+++ API/SaveExp.php
@@ -8,12 +8,14 @@ require_once('../explanation/explanation.php');
 $explanation = new Explanation();
 
 if (filter_input(INPUT_POST, 'gameID') && filter_input(INPUT_POST, 'playerID') && filter_input(INPUT_POST, 'explanation')) {
+
+    
     $gameID = (int)($_POST['gameID']);
     $playerID = (int)($_POST['playerID']);
     $exp = $_POST['explanation'];
     $flag = (int)($_POST['flag']);
     $explanation->AddExplanation($gameID, $playerID, $exp, $flag);
-    $result = true;
+    $result = true
 }
 
 echo json_encode($result);
