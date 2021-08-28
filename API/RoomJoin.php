320918358126b41c20b6ecc9a6980684f62b27a9
nemuki <nemuki@users.noreply.github.com>
2021-08-28 09:28:03 +0000

    php-cs-fixer by Github Actions

--- API/RoomJoin.php
+++ API/RoomJoin.php
@@ -1,9 +1,9 @@
 <?php
 
-header("Access-Control-Allow-Origin:http://localhost");
-header("Content-Type: application/json; charset=utf-8");
+header('Access-Control-Allow-Origin:http://localhost');
+header('Content-Type: application/json; charset=utf-8');
 
-require_once('../room_connect/room.php');
+require_once '../room_connect/room.php';
 
 $room = new Room();
 
@@ -13,7 +13,7 @@ if (filter_input(INPUT_POST, 'roomID') && filter_input(INPUT_POST, 'userID')) {
     $count = count($room->RoomInfo($roomID));
 
 
-    if ($count != 0) {
+    if (0 !== $count) {
         $gameID = $room->RoomInfo($roomID)[0]['gameID'];
         $room->AddRoom($gameID, $userID, $roomID);
         $result = $room->RoomInfo($roomID)[$count];
