d2f391ee3a0ada8d1fd91ddf167e4e7351b35164
KubotaShunsuke <s.kubota0921@gmail.com>
2021-08-28 06:10:15 +0000

    いいね機能を作成 #40

---
+++ API/Good.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json; charset=utf-8");
+require_once('../lib/Library.php');
+$library = new library();
+
+if (filter_input(INPUT_POST, 'libraryID')) {
+    $libraryID = (int)$_POST['libraryID'];
+    $result = $library->Good($libraryID);
+    $responce = 200;
+} else {
+    $result = array('state'=>'リクエストした値が指定している形式と異なる');
+    $responce = 400;
+}
+echo json_encode($result);
+http_response_code($responce);
