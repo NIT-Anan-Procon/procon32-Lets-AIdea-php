7b5f7b458ceee3eb52565f5da6f679e6a9183752
kinoshita0923 <sota0923ks15@gmail.com>
2021-08-29 00:08:38 +0000

    写真を取得するAPIを作成しました。

---
+++ API/GetImg.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json; charset=utf-8");
+
+require_once('../lib/Picture.php');
+require_once('../lib/UserInfo.php');
+require_once('../lib/Room.php');
+
+$room = new Room();
+$picture = new Picture();
+$userInfo = new userInfo();
+
+if ($userInfo->CheckLogin() === false) {
+    echo json_encode(array('state' => 'ログインしていません'));
+    http_response_code(403);
+    exit;
+}
+
+$userID = $userInfo->CheckLogin()['userID'];
+$playerID = $room->getGameInfo($userID)['playerID'];
+$result = $picture->GetGameInfo($playerID);
+
+echo json_encode($result);
+http_response_code(200);
