d44c8b66211f8389a3369ee676789754163951be
kinoshita0923 <kinoshita0923@users.noreply.github.com>
2021-08-29 00:59:00 +0000

    php-cs-fixer by Github Actions

--- API/SavePicture.php
+++ API/SavePicture.php
@@ -25,7 +25,7 @@ if (false === $userInfo->CheckLogin()) {
 $userID = $userInfo->CheckLogin()['userID'];
 $gameID = $room->getGameInfo($userID)['gameID'];
 $infos = $room->gameInfo($gameID);
-$result = array();
+$result = [];
 $i = 0;
 foreach ($infos as $info) {
     $playerID = $info['playerID'];
@@ -55,8 +55,8 @@ foreach ($infos as $info) {
         }
         $picture->AddGameInfo($gameID, $playerID, $url, 0);
     }
-    $result += array($i => $picture->GetGameInfo($playerID));
-    $i++;
+    $result += [$i => $picture->GetGameInfo($playerID)];
+    ++$i;
 }
 
 echo json_encode($result);
