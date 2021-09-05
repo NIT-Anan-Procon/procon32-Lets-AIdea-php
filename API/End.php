bb12c410aff891e9c7856b8d255556f4e951eddc
KubotaShunsuke <KubotaShunsuke@users.noreply.github.com>
2021-09-05 05:42:49 +0000

    php-cs-fixer by Github Actions

--- API/End.php
+++ API/End.php
@@ -39,25 +39,25 @@ if (false === $user['room']) {
 $game = $room->GameInfo($user['room']['gameID']);
 $playerNum = count($game);
 $max = -1;
-for($i = 1; $i <= $playerNum; $i++){
+for ($i = 1; $i <= $playerNum; ++$i) {
     $vote[$i] = $point->getPoint($user['room']['gameID'], $i, 2);
-    if($max < $vote[$i]){
+    if ($max < $vote[$i]) {
         $max = $vote;
         $winner = $i;
     }
 }
-for($i = 1; $i <= $playerNum; $i++){
-    if($vote[$i] == $max && $i != $winner){
+for ($i = 1; $i <= $playerNum; ++$i) {
+    if ($vote[$i] === $max && $i !== $winner) {
         //引き分け！！
     }
 }
-if($winner == 0){
+if (0 === $winner) {
     $result = $userInfo(1);     //AI
 } else {
     $result = $userInfo->getUserInfo($game[$winner - 1]['userID']);
 }
-unset($result['userID']);
-unset($result['password']);
+unset($result['userID'], $result['password']);
+
 $result['explanation'] = $word->getWord($user['room']['gameID'], $winner, 0);
 $result['ng'] = $word->getWord($user['room']['gameID'], $winner, 2);
 if ($user['room']['gamemode'] >= 100) {
@@ -65,7 +65,7 @@ if ($user['room']['gamemode'] >= 100) {
 } else {
     $result['pictureURL'] = $picture->getPicture($user['room']['gameID'], $winner, 1)[0];
 }
-if (1 === (int)$user['room']['flag']) {
+if (1 === (int) $user['room']['flag']) {
 //    $library->UploadLibrary();
 }
 echo json_encode($result);
