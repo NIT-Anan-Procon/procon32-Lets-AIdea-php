3529e21bdd6ae0e0802540f97d30d9ee6524cef8
KubotaShunsuke <s.kubota0921@gmail.com>
2021-09-05 23:57:18 +0000

    処理をまとめて少なくした #59

--- API/Quiz/AddPoint.php
+++ API/Quiz/AddPoint.php
@@ -12,24 +12,24 @@ require_once '../lib/UserInfo.php';
 $point = new Point();
 $room = new Room();
 $userInfo = new UserInfo();
-if (false === $userInfo->CheckLogin()) {
+$user['userInfo'] = $userInfo->CheckLogin();
+if (false === $user['userInfo']) {
     header('Error:Login failed.');
     http_response_code(403);
 
     exit;
 }
-$userID = $userInfo->CheckLogin()['userID'];
-if (false === $room->getGameInfo($userID)) {
+$user['room'] = $room->getGameInfo($userID);
+if (false === $user['room']) {
     header('Error:The user is not in the room.');
     http_response_code(403);
 
     exit;
 }
-$user = $room->getGameInfo($userID);
 if (filter_input(INPUT_POST, 'playerID')) {
     $playerID = (int) $_POST['playerID'];
-    $result['ans'] = $point->AddPoint($user['gameID'], $playerID, 1, 0);
-    $result['exp'] = $point->AddPoint($user['gameID'], $user['playerID'], 1, 1);
+    $result['ans'] = $point->addPoint($user['room']['gameID'], $playerID, 10, 0);
+    $result['exp'] = $point->addPoint($user['room']['gameID'], $user['room']['playerID'], 10, 1);
     if (false === $result['ans'] || false === $result['exp']) {
         http_response_code(400);
     } else {
