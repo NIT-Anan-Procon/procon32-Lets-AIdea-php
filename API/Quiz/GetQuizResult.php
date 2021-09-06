2546f9e9b653e82a9250c9b34d7bda53b3fb5f32
kinoshita0923 <kinoshita0923@users.noreply.github.com>
2021-09-06 05:47:52 +0000

    php-cs-fixer by Github Actions

--- API/Quiz/GetQuizResult.php
+++ API/Quiz/GetQuizResult.php
@@ -17,7 +17,7 @@ $userInfo = new UserInfo();
 $point = new Point();
 $word = new Word();
 
-if(false === $userInfo->CheckLogin()) {
+if (false === $userInfo->CheckLogin()) {
     header('Error: Login failed.');
     http_response_code(403);
 
@@ -35,7 +35,8 @@ if (false === $gameInfo) {
     exit;
 }
 
-function sortByKey($key_name, $sort_order, $array) {
+function sortByKey($key_name, $sort_order, $array)
+{
     foreach ($array as $key => $value) {
         $standard_key_array[$key] = $value[$key_name];
     }
@@ -48,25 +49,25 @@ function sortByKey($key_name, $sort_order, $array) {
 $gameID = $gameInfo['gameID'];
 $roomInfo = $room->RoomInfo($gameInfo['roomID']);
 $result = [];
-for($i = 0; $i < count($roomInfo); $i++) {
+for ($i = 0; $i < count($roomInfo); ++$i) {
     $explanation = $word->getWord($gameID, $roomInfo[$i]['playerID'], 0);
     $exp = $point->GetPoint($roomInfo[$i]['playerID'], 0);
     $ans = $point->GetPoint($roomInfo[$i]['playerID'], 1);
     $user = $userInfo->GetUserInfo($roomInfo[$i]['userID']);
-    $array = array(
-        'name'          => $user['name'],
-        'icon'          => $user['icon'],
-        'badge'         => $user['badge'],
-        'explanation'   => $explanation,
-        'exp'           => (int)($exp['SUM(pointNum)']),
-        'ans'           => (int)($ans['SUM(pointNum)']),
-        'sum'           => (int)($exp['SUM(pointNum)']) + (int)($ans['SUM(pointNum)'])
-    );
-    array_push($result, $array);
+    $array = [
+        'name' => $user['name'],
+        'icon' => $user['icon'],
+        'badge' => $user['badge'],
+        'explanation' => $explanation,
+        'exp' => (int) ($exp['SUM(pointNum)']),
+        'ans' => (int) ($ans['SUM(pointNum)']),
+        'sum' => (int) ($exp['SUM(pointNum)']) + (int) ($ans['SUM(pointNum)']),
+    ];
+    $result[] = $array;
 }
 
 $result = sortByKey('sum', SORT_DESC, $result);
-for($i = 0; $i < count($roomInfo); $i++) {
+for ($i = 0; $i < count($roomInfo); ++$i) {
     unset($result[$i]['sum']);
 }
 
