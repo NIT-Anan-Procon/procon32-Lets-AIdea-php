df55257d4bc1d74bc8def104376d1d0b74733057
kinoshita0923 <sota0923sk15@gmail.com>
2021-09-09 14:16:13 +0000

    headerを修正

--- API/Quiz/GetQuizResult.php
+++ API/Quiz/GetQuizResult.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set('display_errors', 1);
-header('Access-Control-Allow-Origin:http://localhost');
+header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json; charset=utf-8');
 
 require_once '../../lib/Room.php';
