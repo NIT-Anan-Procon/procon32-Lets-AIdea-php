879edf5c119b062c2bec7b78ad7f43f5438f1973
kinoshita0923 <sota0923sk15@gmail.com>
2021-09-18 11:38:01 +0000

    アイコンを設定する処理を追加しました。

--- API/User/CreateUser.php
+++ API/User/CreateUser.php
@@ -3,15 +3,20 @@
 ini_set('display_errors', 1);
 
 require_once '../../lib/UserInfo.php';
+
+require_once '../../lib/UnsplashApi.php';
 header('Access-Control-Allow-Origin:'.URL);
 header('Access-Control-Allow-Credentials:true');
 
 $userInfo = new userInfo();
+$unsplash = new UnsplashApi();
 
 if (filter_input(INPUT_POST, 'name') && filter_input(INPUT_POST, 'password')) {
     $name = (string) $_POST['name'];
     $password = (string) $_POST['password'];
-    $icon = $_POST['icon'];
+    $word = ['animal','art','scenery','people'];
+    $key_word = array_rand($word,1);
+    $icon = $unsplash->getPhoto($key_word);
     $result = $userInfo->AddUserInfo($name, $password, $icon);
     if (false === $result['character']) {
         header('Error:Your name and password must be alphanumeric.');
